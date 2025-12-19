cefb62ac7f5afd820df57cf4b2e38f98673b5312
mouade-99 <ennacirimouade1@gmail.com>
2025-12-19 19:32:26 +0100

    products and supports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import HeroPart1 from "../components/Hero-part1";
+import HeroPart1 from "../components/home-components/Hero-part1";
+import Products from "./Products";
 function Home(){
   return <div className=" bg-[#1b1b1b]">
     <HeroPart1/>
