dad493e05f315571b17c566d5b3ccd6158c37b75
Kival <Abdelkhalek>
2025-12-22 01:00:26 +0100

    initial commit with project files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SupportHero from './SupportHero'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="bg-[url('bg-img-colayb.jpg')] bg-center min-h-screen bg-no-repeat bg-cover">
+      <SupportHero />
+    </div>
+  )
+}
+
+export default App
